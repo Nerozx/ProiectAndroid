9f39dea6b2a03530b522d4ae36bd2470b417a2ca
Nerozx <mirceasc8@gmail.com>
2016-12-13 21:05:54 +0200

    minor changes

--- app/src/main/java/com/example/mircea/proiectandroid/MainActivity.java
+++ app/src/main/java/com/example/mircea/proiectandroid/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mircea.proiectandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
         login_button=(Button)findViewById(R.id.login_button);
         username_field=(EditText)findViewById(R.id.username_field);
         password_field=(EditText)findViewById(R.id.password_field);
+        password_field.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //de aici poti sa iei textul inserat in pagina de login pentru baza de date
+                Log.i("Username",username_field.getText().toString());
+                Log.i("Password",password_field.getText().toString());
+            }
+        });
     }
 }
