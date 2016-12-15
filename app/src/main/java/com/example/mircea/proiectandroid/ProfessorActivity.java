98489f72088b6ead72107a9b94d19a6ecd66b978
Nerozx <mirceasc8@gmail.com>
2016-12-15 16:36:31 +0200

    Implemented the basic create test window

--- app/src/main/java/com/example/mircea/proiectandroid/ProfessorActivity.java
+++ app/src/main/java/com/example/mircea/proiectandroid/ProfessorActivity.java
@@ -1,13 +1,28 @@
 package com.example.mircea.proiectandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfessorActivity extends AppCompatActivity {
+
+    private Button search_btn;
+    private Button create_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor);
+        search_btn=(Button) findViewById(R.id.button_search);
+        create_btn=(Button) findViewById(R.id.button_create_test);
+        create_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent new_activity=new Intent(ProfessorActivity.this,CreateTestActivity.class);
+                ProfessorActivity.this.startActivity(new_activity);
+            }
+        });
     }
 }
