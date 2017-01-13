c6648c9827d4cfc3b8b564e96f0d17d2f07d1a0c
Nerozx <mirceasc8@gmail.com>
2017-01-13 23:00:02 +0200

    a

---
+++ app/src/main/java/com/example/mircea/proiectandroid/TestErrorDialog.java
@@ -0,0 +1,16 @@
+package com.example.mircea.proiectandroid;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+
+/**
+ * Created by Mircea on 13/1/2017.
+ */
+
+public class TestErrorDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
