8f0521abfcf988bca95c8cabfd6e30acdbb7063d
Nerozx <mirceasc8@gmail.com>
2017-01-14 11:23:52 +0200

    bugfixes

--- app/src/main/java/com/example/mircea/proiectandroid/CreateTestActivity.java
+++ app/src/main/java/com/example/mircea/proiectandroid/CreateTestActivity.java
@@ -1,6 +1,8 @@
 package com.example.mircea.proiectandroid;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -45,11 +47,13 @@ public class CreateTestActivity extends AppCompatActivity {
         addQuestion_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 EditText questionzone=new EditText(CreateTestActivity.this);
                 Switch multipleans_switch=new Switch(CreateTestActivity.this);
                 Button add_answer=new Button(CreateTestActivity.this);
                 questionzone.setHint("Insert Question Here");
                 add_answer.setText("Add Answer");
+
                 multipleans_switch.setText("Multiple Correct Answer Question?");
 
                 add_answer.setOnClickListener(new View.OnClickListener() {
@@ -60,14 +64,16 @@ public class CreateTestActivity extends AppCompatActivity {
                         answerzone.setHint("Insert Answer Here!");
                         Switch isCorrect=new Switch(CreateTestActivity.this);
                         isCorrect.setText("Correct Answer!");
-                        linearLayout.addView(isCorrect);
-                        linearLayout.addView(answerzone);
+                        int i=linearLayout.indexOfChild(view);
+                                    linearLayout.addView(isCorrect,i+1);
+                                    linearLayout.addView(answerzone,i+2);
                     }
 
                 });
                 linearLayout.addView(multipleans_switch);
                 linearLayout.addView(questionzone);
                 linearLayout.addView(add_answer);
+
             }
         });
 
@@ -75,11 +81,29 @@ public class CreateTestActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 getData();
-                if(lst_err==null){
+                if(lst_err.isEmpty()){
                     Intent new_activity=new Intent(CreateTestActivity.this,TestSuccessActivity.class);
                     CreateTestActivity.this.startActivity(new_activity);
                 }else{
+                    int i=0;
+                    String x="";
+                    AlertDialog.Builder builder=new AlertDialog.Builder(CreateTestActivity.this);
 
+                    builder.setIcon(android.R.drawable.ic_dialog_alert);
+                    for (String s:lst_err) {
+                        i++;
+
+                        x=x+i+"."+s+"\n";
+                    }
+                    builder.setMessage(x);
+                    builder.setTitle(i+" Error(s)!");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            lst_err.clear();
+                        }
+                    });
+                    builder.show();
                 }
             }
         });
@@ -116,21 +140,24 @@ public class CreateTestActivity extends AppCompatActivity {
                     if(e.getText().length()!=0) {
                         question_no++;
                         testQuestion.setQuestion_text(e.getText().toString());
+                        lst_question.add(testQuestion);
                         Log.i("QUESTION", testQuestion.getQuestion_text());
                     }else{
                         question_no++;
                         String question_err="You have question number "+question_no+ "left blank";
                         lst_err.add(question_err);
                     }
-                }else if(e.getHint().equals("Insert Answer Here!")){
-                    answer_no++;
-                    testAnswer.setAnswer_text(e.getText().toString());
-                    lst_answers.add(testAnswer);
-                    Log.i("ANSWER",testAnswer.getAnswer_text());
-                }else{
-                    answer_no++;
-                    String answer_err="You have answer number"+answer_no + "left blank from question"+question_no;
-                    lst_err.add(answer_err);
+                }else if(e.getHint().equals("Insert Answer Here!")) {
+                    if (e.getText().length() != 0) {
+                        answer_no++;
+                        testAnswer.setAnswer_text(e.getText().toString());
+                        lst_answers.add(testAnswer);
+                        Log.i("ANSWER", testAnswer.getAnswer_text());
+                    } else {
+                        answer_no++;
+                        String answer_err = "You have answer number " + answer_no + " left blank from question" + question_no;
+                        lst_err.add(answer_err);
+                    }
                 }
             }else if(v instanceof Switch){
                 Switch f=(Switch) v;
@@ -139,7 +166,6 @@ public class CreateTestActivity extends AppCompatActivity {
                     testQuestion.setQuestion_no_ans(f.isChecked() ? 1:0);
                 }else if(f.getText().equals("Multiple Correct Answer Question?") && question_no>=1){
                     testQuestion.setQuestion_answer_list(lst_answers);
-                    lst_question.add(testQuestion);
                     testQuestion=new TestQuestion();
                     lst_answers=new ArrayList<>();
                     answer_no=0;
@@ -154,6 +180,9 @@ public class CreateTestActivity extends AppCompatActivity {
 
             }
         }
+        if(question_no==1){
+            testQuestion.setQuestion_answer_list(lst_answers);
+        }
         checkTest(lst_err,lst_question);
         choiceTest.setTest_question_lst(lst_question);
         choiceTest.setTest_question_no(question_no);
@@ -162,31 +191,54 @@ public class CreateTestActivity extends AppCompatActivity {
 
 
 private void checkTest(List<String> lst_err, List<TestQuestion> lst_question) {
-    if (lst_question != null) {
+    if (!lst_question.isEmpty()) {
         for (TestQuestion question : lst_question) {
             if (question.getQuestion_no_ans() == 1) {
-                int no_correct = 0;
-                for (TestAnswer answer : question.getQuestion_answer_list()) {
-                    if (answer.isAnswer_right() == 1) {
-                        no_correct++;
+
+                if(!question.getQuestion_answer_list().isEmpty()) {
+                    int no_correct = 0;
+                    int no_answers=0;
+                    for (TestAnswer answer : question.getQuestion_answer_list()) {
+                        no_answers++;
+                        if (answer.isAnswer_right() == 1) {
+                            no_correct++;
+                        }
                     }
-                }
-                if (no_correct == 0) {
-                    String err = "You have questions with no answers!";
-                    lst_err.add(err);
-                } else if (no_correct == 1) {
-                    String err = "Please mark 2 or more answers as correct at multiple answer question!";
-                    lst_err.add(err);
-                }
-            }else{
-                int no_correct=0;
-                for (TestAnswer answer : question.getQuestion_answer_list()) {
-                    if (answer.isAnswer_right() == 1) {
-                        no_correct++;
+                    if (no_correct == 0) {
+                        String err = "You have questions with no correct answers!";
+                        lst_err.add(err);
+                    } else if (no_correct == 1) {
+                        String err = "Please mark 2 or more answers as correct at multiple answer question!";
+                        lst_err.add(err);
                     }
+                    if(no_answers==1){
+                        String err="Please add more than one answer to your question(s)";
+                        lst_err.add(err);
+                    }
+                }else{
+                    String err="Please add answers to your question(s)";
+                    lst_err.add(err);
                 }
-                if(no_correct==0){
-                    String err="You have single answer questions with no answers";
+            }else {
+                if (!question.getQuestion_answer_list().isEmpty()) {
+                    int no_correct = 0;
+                    int no_answers=0;
+                    for (TestAnswer answer : question.getQuestion_answer_list()) {
+                        no_answers++;
+                        if (answer.isAnswer_right() == 1) {
+                            no_correct++;
+                        }
+                    }
+                    if (no_correct == 0) {
+                        String err = "You have single answer questions with no correct answers";
+                        lst_err.add(err);
+                    }
+                    if(no_answers==1){
+                        String err="Please add more than one answer to your question(s)";
+                        lst_err.add(err);
+                    }
+                }else{
+                    String err="Please add answers to your question(s)";
                     lst_err.add(err);
                 }
             }
--- app/src/main/java/com/example/mircea/proiectandroid/TestErrorDialog.java
+++ app/src/main/java/com/example/mircea/proiectandroid/TestErrorDialog.java
@@ -1,7 +1,9 @@
 package com.example.mircea.proiectandroid;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 /**
@@ -11,6 +13,15 @@ import android.os.Bundle;
 public class TestErrorDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
 }
