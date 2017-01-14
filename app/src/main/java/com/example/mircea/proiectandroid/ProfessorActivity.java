a8fd17d5308618c072fed40ff5285698ac1152ba
Palti <paltineanu13@gmail.com>
2017-01-14 18:52:40 +0200

    iu

--- app/src/main/java/com/example/mircea/proiectandroid/ProfessorActivity.java
+++ app/src/main/java/com/example/mircea/proiectandroid/ProfessorActivity.java
@@ -7,13 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.mircea.proiectandroid.database.LoginUtility;
 import com.example.mircea.proiectandroid.model.Users;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfessorActivity extends AppCompatActivity {
 
     private Button search_btn;
     private Button create_btn;
     private TextView greeting;
+    private List<Users> lst_stud = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +42,25 @@ public class ProfessorActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                LoginUtility loginUtility = new LoginUtility(ProfessorActivity.this);
+                loginUtility.openDB();
+                lst_stud=loginUtility.getStudents("1");
+                loginUtility.closeDB();
+                List<String> lst_users=new ArrayList<String>();
+                List<String> lst_users_id=new ArrayList<String>();
+                for (Users u: lst_stud) {
+                    lst_users.add(u.getUser_name());
+                    lst_users_id.add((String.valueOf(u.getUser_id())));
+                }
+                String[] stud = new String[lst_users.size()];
+                stud = lst_users.toArray(stud);
+                String[] id=new String[lst_users_id.size()];
+                id=lst_users_id.toArray(id);
+
 
                 Intent new_activity=new Intent(ProfessorActivity.this,SearchStudActivity.class);
+                new_activity.putExtra("stud", stud);
+                new_activity.putExtra("id",id);
                 ProfessorActivity.this.startActivity(new_activity);
             }
         });
