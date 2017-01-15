b17ea41587161590fb21a96b0fca1bd73cb4e2eb
Palti <paltineanu13@gmail.com>
2017-01-15 22:46:27 +0200

    iu

--- app/src/main/java/com/example/mircea/proiectandroid/ProfessorActivity.java
+++ app/src/main/java/com/example/mircea/proiectandroid/ProfessorActivity.java
@@ -3,6 +3,7 @@ package com.example.mircea.proiectandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -65,5 +66,14 @@ public class ProfessorActivity extends AppCompatActivity {
                 ProfessorActivity.this.startActivity(new_activity);
             }
         });
+    }
+    @Override
+    public boolean onKeyDown ( int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent new_activity = new Intent(ProfessorActivity.this, MainActivity.class);
+            ProfessorActivity.this.startActivity(new_activity);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
